b16060b1dddbf0eb3b0b1273d85ef6394b535ba3
Jacob(bad at programming) <66473431+VeryBadAtPython@users.noreply.github.com>
2022-05-24 12:12:44 +1000

    Finished HigherOrder.hs

--- HigherOrder.hs
+++ HigherOrder.hs
@@ -7,35 +7,45 @@ module HigherOrder where
 
 -- arithMean : Computes the average of a list of numbers.
 arithMean :: (Fractional a) => [a] -> a
-arithMean = undefined
+arithMean list = (foldr (+) 0 list)/(fromIntegral (length list))
+
+
 
 -- Computes the geometric mean of a list of numbers.
 geoMean :: (Floating a) => [a] -> a
-geoMean = undefined
+geoMean list = (foldr (+) 0 list)/(fromIntegral (length list))
 
 -- l1Norm : Takes a list of numbers, and computes the sum of the 
 -- absolute values of each term.
 l1Norm :: (Num a) => [a] -> a
-l1Norm = undefined
+l1Norm list = foldr (+) 0 (map abs list)
 
--- l2Norm : Takes a list of numbers, and computes the Eucledian norm,
+-- l2Norm : Takes a list of numbers, and computes the Euclidean norm,
 -- defined to be the square root of the sum of the squares of each element.
 l2Norm :: (Floating a) => [a] -> a 
-l2Norm = undefined
+l2Norm list = sqrt $ foldr (+) 0 $ map (\x->x*x) list
 
 -- cesaroSum : Takes a list of numbers, and returns a list, where the nth element
 -- is the average of the first n terms of the input list.
 cesaroSum :: (Fractional a) => [a] -> [a]
-cesaroSum = undefined
+cesaroSum list = case list of
+    []   -> []
+    x:xs -> (cesaroSum xs)++[arithMean (x:xs)]
+
 
 -- ===========================================
 
 
 -- | revMap : Takes an element, and feeds that element to each function in a list.
 revMap :: a -> [(a -> b)] -> [b]
-revMap = undefined
+revMap e l = case l of
+    []   -> []
+    f:fs -> (f e):revMap e fs
 
 -- | superMap : Takes a list of elements, and a list of functions, and applies each element to
 -- | each function. Your output should be a list of lists.
 superMap :: [a] -> [(a -> b)] -> [[b]]
-superMap = undefined
+superMap eList fList = case eList of
+    [] -> []
+    e:es -> (revMap e fList):(superMap es fList)
+
