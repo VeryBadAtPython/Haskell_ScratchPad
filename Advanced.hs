177bd0d2e84b577a06150fa3d50b41c928025788
Jacob(bad at programming) <66473431+VeryBadAtPython@users.noreply.github.com>
2022-06-03 20:21:24 +1000

    Done polyeval

--- Advanced.hs
+++ Advanced.hs
@@ -4,12 +4,18 @@ module Advanced where
 
 -- polyEval [a0, a1, a2, ..., an] x = a0 + a1*x + a2*x^2 + ... an*x^n
 polyEval :: [Double] -> Double -> Double
-polyEval = undefined
+polyEval coeffs val = evaluator coeffs val 0
+
+evaluator :: [Double] -> Double -> Double -> Double
+evaluator [] _ _ = 0
+evaluator (x:xs) val pwr = x*val**pwr + evaluator xs val (pwr+1)
+
 
 -- | hornerEval : Same as polyEval, but uses Horner's rule
 -- to speed up computation time
 hornerEval :: [Double] -> Double -> Double
-hornerEval = undefined
+hornerEval [] _ = 0
+hornerEval (x:xs) val = x + val*(hornerEval xs val)
 
 -- | isPrime : Checks if a number is prime. Try to be efficient!
 isPrime :: Integer -> Bool
